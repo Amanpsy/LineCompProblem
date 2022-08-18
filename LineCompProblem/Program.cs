859dc045a3eb5e9197545dd147ae3d6bfb8c8cf8
Aman <amankr97111@gmail.com>
2022-08-18 15:16:56 +0530

    first commit

---
+++ LineCompProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the value of X1 and Y1 coordinates");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the value of X2 and Y2 coordinates");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+
+            var distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine("the distance between the two points is " + distance);
+        }
+
+
+
+    }
+}
